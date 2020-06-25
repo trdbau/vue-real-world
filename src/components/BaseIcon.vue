7fa71961af05d3baeaa5154977a738da664f71e3
TranDuyBau <trdbau@gmail.com>
2020-06-25 22:43:57 +0700

    Global Vue Icon

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="icon-wrapper">
+    <svg class="icon" width="" height="">
+      <use v-bind="{ 'xlink:href': '/feather-sprite.svg#' + name }" />
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style></style>
---
+++ src/views/User.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Hello {{ username }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    username: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
